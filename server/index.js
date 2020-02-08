4a10adbca124222bd5e8867082ea4153c40debe9
Vlad Ilinyh <v.ilinyh@gmail.com>
2020-02-09 01:28:54 +0600

    server response modified

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send(generator.generate({
-        length: 5,
+        length: 10,
         charset: 'alphabetic'
     }));
 });
